0086af836ef5858605e5761e1b958d8abcf640fe
Luyuan Wang <mail@luyuan.wang>
2022-11-08 21:22:17 -0500

    after project proposal

--- random_agent_test.py
+++ random_agent_test.py
@@ -3,18 +3,24 @@ from PIL import Image
 import gym
 import d4rl # Import required to register environments
 
+import matplotlib.pyplot as plt
+
 # Create the environment
-env = gym.make('antmaze-umaze-diverse-v2')
+env = gym.make('antmaze-umaze-diverse-custom-v2')
 
 # d4rl abides by the OpenAI gym interface
-env.reset()
 
-frames = []
-for i in range(1000):
-    frame = env.render(mode='rgb_array')
-    frames.append(Image.fromarray(frame))
+total_rewards = []
+for _ in range(100):
+    obs = env.reset()
+    total_reward = 0
+    for i in range(1000):
+        obs, reward, done, info = env.step(env.action_space.sample())
+        total_reward += reward
 
-    env.step(env.action_space.sample())
+    total_rewards.append(total_reward)
 
-env.close()
-imageio.mimwrite("./data/random_agent.gif", frames, fps=60)
+plt.scatter(range(len(total_rewards)), total_rewards)
+plt.ylabel("Episode Returns")
+plt.xlabel("Random goal (individual episodes)")
+plt.savefig("./data/fig.png")
